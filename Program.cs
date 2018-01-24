ff58c01bc7062dc6b468eac2de1a2f6521cd7c37
Rian Roberts <rr923979@wtstudents.wtamu.edu>
2018-01-24 12:12:57 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SonofANickNameGeneraotr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[,] nicknames = new string [10,2];
+
+            // row 1, column 1
+            nicknames[0,0] = "Dude";
+            // row 1, column 2
+            nicknames[0,1] = "Jones";
+
+             // row 2, column 1
+            nicknames[1,0] = "Snappy";
+            // row 2, column 2
+            nicknames[1,1] = "Firecracker";
+
+
+            Console.WriteLine("Please enter your name");
+            
+            string name = Console.ReadLine();
+
+            char[] nameparts = name.ToCharArray();
+
+            for(int i = 0; i < nameparts.Length; i++){
+                
+            }
+
+
+        }
+    }
+}
